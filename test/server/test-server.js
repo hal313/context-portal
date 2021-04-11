312fe7af918e01d2498991e2ed91146b07cf0569
John Ghidiu <john-github@rmdashrf.org>
2021-04-11 16:51:45 -0400

    Commented test server

--- test/server/test-server.js
+++ test/server/test-server.js
@@ -1,6 +1,12 @@
 import * as ServeHandler from 'serve-handler';
 import * as http from 'http';
 
+// This file sets up an HTTP server which will help with automated testing. Because the original tests are in Mocha,
+// this will allow for automated testing by serving the pages and avoiding any cross origin restrictions. This can
+// also be used during development in the case where the current IDE does not live serve pages.
+//
+// Sending a request to '/stop' will stop the server and terminate the process (useful on CI servers)
+
 // Get the handler
 const handler = ServeHandler.default;
 
